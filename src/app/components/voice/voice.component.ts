0adf3e359440e61a682938e43ec9c2a4fbccd784
ProPanesar <panesar.pb08@gmail.com>
2020-09-29 22:38:16 -0400

    init

--- src/app/components/voice/voice.component.ts
+++ src/app/components/voice/voice.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
 import { VoiceService } from '../../shared/voice.service';
 
 @Component({
@@ -11,35 +10,46 @@ export class VoiceComponent implements OnInit, OnDestroy {
 
 	@Output() close = new EventEmitter();
 
+	private recognition = new window['webkitSpeechRecognition'];
+	
+	transcript: string = '';
 	isListening: boolean = true;
-	transcript: string;
 
-	private subscription: Subscription;
-
-	constructor(public voiceService: VoiceService) { 
-		voiceService.init();
-	}
+	constructor(private voiceService: VoiceService) { }
 
 	ngOnInit(): void {
 		this.start();
-		this.subscription = this.voiceService.getUserSpeech()
-			.subscribe(transcript => this.transcript = transcript);
+		this.recognition.addEventListener('result', (e: SpeechRecognitionEvent) => {
+			this.transcript = Array.from(e.results)
+				.map((result) => result[0])
+				.map((result) => result.transcript)
+				.join('');
+		});
+		this.recognition.addEventListener('end', () => {
+			if (this.isListening) {
+				this.recognition.stop();
+			}
+			this.isListening = false;
+		});
 	}
 
 	start(): void {
-	 	this.voiceService.start();
 		this.isListening = true;
+		this.recognition.start();
 	}
-  
+
 	stop(): void {
-		this.voiceService.stop();
-		this.isListening = false;
+		this.recognition.stop();
+	}
+
+	verifyTranscript(): void {
+		if (this.transcript === '' || this.transcript === ' ') {
+			this.transcript = 'Invalid Command, Please try again'
+		}
 	}
 
 	ngOnDestroy(): void {
 		this.stop();
-		this.transcript = '';
-		this.subscription.unsubscribe();
 		this.close.emit();
 	}
 
--- src/app/shared/voice.service.ts
+++ src/app/shared/voice.service.ts
@@ -5,40 +5,6 @@ import { EmployeeService } from './employee.service';
 @Injectable({ providedIn: 'root' })
 export class VoiceService {
 
-	private recognition = new window['webkitSpeechRecognition'];
-	private transcript = new BehaviorSubject<string>('');
-	private isStoppedSpeechRecog: boolean;
-
 	constructor(private employeeService: EmployeeService) { }
-
-	init(): void {
-		this.recognition.addEventListener('result', (e: SpeechRecognitionEvent) => {
-			const transcript = Array.from(e.results)
-				.map((result) => result[0])
-				.map((result) => result.transcript)
-				.join('');
-			console.log(transcript);
-			this.transcript.next(transcript);
-		});
-	}
-
-	start() {
-		this.isStoppedSpeechRecog = false;
-		this.recognition.start();
-		this.recognition.onend = () => {
-			if (this.isStoppedSpeechRecog) {
-				this.recognition.stop();
-			}
-		};
-	}
-	
-	stop() {
-		this.isStoppedSpeechRecog = true;
-		this.recognition.stop();
-	}
-
-	getUserSpeech(): Observable<string> {
-		return this.transcript.asObservable();
-	}
 
 }
